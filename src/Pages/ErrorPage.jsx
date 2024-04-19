7e255cd476549215b33d298353bdc8a44d5601ee
Darshan Pandya <andyadarshan811@gmail.com>
2024-04-19 22:16:27 +0530

    [Add] Error Page.

--- src/Pages/ErrorPage.jsx
+++ src/Pages/ErrorPage.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const ErrorPage = () => {
   return (
-    <div>
-      <h1>Error Page</h1>
+    <div className="flex mx-auto items-center justify-center m-6 w-fit h-fit p-4 ">
+      <div className="text-center">
+        <h1 className="text-2xl md:text-4xl font-bold mb-4">Oops! Page Not Found</h1>
+        <p className="text-lg md:text-xl mb-8">The page you are looking for does not exist.</p>
+        <Link to="/" className="font-semibold tracking-wider hover:text-blue-700 text-lg">Go back to homepage.</Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ErrorPage
+export default ErrorPage;
